c9da4b8c5d71c2e5cb9757c4c58c669fa59dd5b3
Magnus <magnus.nistad.helvig@gmail.com>
2024-12-04 12:00:54 +0100

    Background scroll+clone

--- Assets/Scripts/Obstacles/ChunkScroll.cs
+++ Assets/Scripts/Obstacles/ChunkScroll.cs
@@ -3,11 +3,21 @@ using UnityEngine;
 public class ChunkScroll : MonoBehaviour
 {
     public float ScrollSpeed = 5f;
-
-
+    
+    public GameObject Chunk;
+    public Transform spawnPoint;
+    
+    //Checks if this chunk spawned the next chunk?
+    private bool spawned = false;
+    
     void Update()
     {
         transform.Translate(Vector2.down * ScrollSpeed * Time.deltaTime);
+        if (transform.position.y > 0 && !spawned)
+        {
+            Instantiate(Chunk, spawnPoint.position, Quaternion.identity);
+            spawned = true;
+        }
     }
 }
 //Chunk should be despawned after set time in when instantiated
