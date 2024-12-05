4c413328f14bbb75b4e5f2be9b7ed6a9fb37f2f4
Magnus <magnus.nistad.helvig@gmail.com>
2024-12-05 11:49:08 +0100

    RandomChunks

--- Assets/Scripts/Obstacles/ChunkScroll.cs
+++ Assets/Scripts/Obstacles/ChunkScroll.cs
@@ -2,23 +2,12 @@ using UnityEngine;
 
 public class ChunkScroll : MonoBehaviour
 {
-    //Negative for scrolling down
+    //Make negative for scrolling down
     public float ScrollSpeed = 3f;
-    
-    public GameObject Chunk;
-    public Transform spawnPoint;
-    
-    private bool spawned = false;
     
     void Update()
     {
         transform.Translate(Vector2.up * ScrollSpeed * Time.deltaTime);
-        //Checks if this chunk has not yet spawned a chunk
-        if (transform.position.y > -0.1f && !spawned)
-        {
-            Instantiate(Chunk, spawnPoint.position, Quaternion.identity);
-            spawned = true;
-        }
     }
+
 }
-//ADD Chunk should be despawned after set time in when instantiated
---
+++ Assets/Scripts/Obstacles/ChunkSpawn.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChunkSpawn : MonoBehaviour
+{
+    private bool spawnedNextChunk = false;
+
+    //chunkHeight = -10.8 * number of backgrounds
+    public float chunkHeight = -10.8f;
+    
+    //ADD spawn chunks randomly from a list
+    public List<GameObject> chunkList = new List<GameObject>();
+    
+    void FixedUpdate()
+    {
+        //Checks if this chunk has not yet spawned a chunk
+        if (transform.position.y > 0f && !spawnedNextChunk)
+        {
+            var chunkNumber = Random.Range(0, chunkList.Count);
+            print(chunkNumber);
+            
+            var spawnPoint = transform.position + new Vector3(0, chunkHeight, 0);
+            Instantiate(chunkList[chunkNumber], spawnPoint, Quaternion.identity);
+            //Destroy this chunk in 5 seconds
+            Destroy(gameObject, 5f);
+            spawnedNextChunk = true;
+        }
+    }
+}
