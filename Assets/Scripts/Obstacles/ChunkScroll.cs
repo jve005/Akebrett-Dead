1457e8a894a16f5ed8e4f271dd90ae4186ddc12d
Magnus <magnus.nistad.helvig@gmail.com>
2024-12-05 10:47:35 +0100

    Add test backgrounds

--- Assets/Scripts/Obstacles/ChunkScroll.cs
+++ Assets/Scripts/Obstacles/ChunkScroll.cs
@@ -2,22 +2,23 @@ using UnityEngine;
 
 public class ChunkScroll : MonoBehaviour
 {
-    public float ScrollSpeed = 5f;
+    //Negative for scrolling down
+    public float ScrollSpeed = 3f;
     
     public GameObject Chunk;
     public Transform spawnPoint;
     
-    //Checks if this chunk spawned the next chunk?
     private bool spawned = false;
     
     void Update()
     {
-        transform.Translate(Vector2.down * ScrollSpeed * Time.deltaTime);
-        if (transform.position.y > 0 && !spawned)
+        transform.Translate(Vector2.up * ScrollSpeed * Time.deltaTime);
+        //Checks if this chunk has not yet spawned a chunk
+        if (transform.position.y > -0.1f && !spawned)
         {
             Instantiate(Chunk, spawnPoint.position, Quaternion.identity);
             spawned = true;
         }
     }
 }
-//Chunk should be despawned after set time in when instantiated
+//ADD Chunk should be despawned after set time in when instantiated
