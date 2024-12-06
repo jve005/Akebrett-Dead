d92563b8dd64a9806910f984fd009217a0e39ead
Magnus <magnus.nistad.helvig@gmail.com>
2024-12-06 13:29:27 +0100

    Buncha new sprites

--- Assets/Scripts/Obstacles/ChunkSpawn.cs
+++ Assets/Scripts/Obstacles/ChunkSpawn.cs
@@ -7,19 +7,44 @@ public class ChunkSpawn : MonoBehaviour
 
     //chunkHeight = 10.8 * number of backgrounds
     public float chunkHeight = 10.8f;
+
+    public int[] starts = new int[3];
+    public int[] ends = new int[3];
     
-    //ADD spawn chunks randomly from a list
     public List<GameObject> chunkList = new List<GameObject>();
+
+    //Finds a chunk which can merge together with the current one without causing an unstoppable loss
+    private static GameObject FindChunk(int[] ends, List<GameObject> chunkList)
+    {
+        //Pick a random chunk from among the list of chunks
+        var chunkNumber = Random.Range(0, chunkList.Count);
+        
+        //Find the start list numbers from the picked chunk
+        var starts = chunkList[chunkNumber].GetComponent<ChunkSpawn>().starts;
+        var mergable = false;
+        var i = 0;
+        while (i < ends.Length)
+        {
+            if (ends[i] == 0 && starts[i] == 0) { mergable = true; break; }
+            i++;
+        }
+        //Can this and the next chunk merge?
+        if (mergable)
+        {
+            return chunkList[chunkNumber];
+        }
+        //Otherwise redo the function
+        return FindChunk(ends, chunkList);
+    }
     
     void FixedUpdate()
     {
-        //Checks if this chunk has not yet spawned a chunk
+        //Check if this chunk has not yet spawned a chunk
         if (transform.position.y > 0f && !spawnedNextChunk)
         {
-            var chunkNumber = Random.Range(0, chunkList.Count);
-            
+            var nextChunk = FindChunk(starts, chunkList);
             var spawnPoint = transform.position + new Vector3(0, -chunkHeight, 0);
-            Instantiate(chunkList[chunkNumber], spawnPoint, Quaternion.identity);
+            Instantiate(nextChunk, spawnPoint, Quaternion.identity);
             //Destroy this chunk in 5 seconds
             Destroy(gameObject, 5f);
             spawnedNextChunk = true;
