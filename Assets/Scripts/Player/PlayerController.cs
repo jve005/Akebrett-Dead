241290745d2e40a848b22f59c1347835229039af
Magnus <magnus.nistad.helvig@gmail.com>
2024-12-13 10:41:37 +0100

    Lane shift audio added

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,8 +4,9 @@ using UnityEngine.Serialization;
 
 public class PlayerController : MonoBehaviour
 {
-    public Animator animator;
-    public Rigidbody2D rigidBody2D;
+    public Animator _animator;
+    private Rigidbody2D _rigidBody2D;
+    private AudioSource _audio;
     private Vector2 _direction;
     public float moveSpeed = 5f;
     public Transform playerSpot;
@@ -14,7 +15,8 @@ public class PlayerController : MonoBehaviour
     
     void Start()
     {
-        rigidBody2D = GetComponent<Rigidbody2D>();
+        _rigidBody2D = GetComponent<Rigidbody2D>();
+        _audio = GetComponent<AudioSource>();
     }
 
     void Update()
@@ -26,16 +28,18 @@ public class PlayerController : MonoBehaviour
             transform.localScale = transform.position.x > playerSpot.position.x ? 
                 new Vector2(1, 1) : new Vector2(-1, 1);
         }
-        animator.SetFloat("Movement", _direction.x);
+        _animator.SetFloat("Movement", _direction.x);
 
         if (Keyboard.current.aKey.wasPressedThisFrame && _wayPointIndex != 0)
         {
+            _audio.Play();
             _wayPointIndex--;
             playerSpot = wayPoint[_wayPointIndex];
         }
 
         if (Keyboard.current.dKey.wasPressedThisFrame && _wayPointIndex != wayPoint.Length - 1)
         {
+            _audio.Play();
             _wayPointIndex++;
             playerSpot = wayPoint[_wayPointIndex];
         }
@@ -43,6 +47,6 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        rigidBody2D.linearVelocity = _direction * moveSpeed;
+        _rigidBody2D.linearVelocity = _direction * moveSpeed;
     }
 }
